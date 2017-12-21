e2032ce9b890ad2c9986c7234cc3786cb4af45c0
Agung Kurniawan <cintarembo@gmail.com>
2017-12-22 00:26:05 +0700

    Move topbar inside body

--- application/views/layouts/public/headers.php
+++ application/views/layouts/public/headers.php
@@ -45,8 +45,7 @@
       <link rel="stylesheet" href="<?php echo base_url()?>public/assets/vendor/bootstrap/css/bootstrap.min.css">
       <link rel="stylesheet" href="<?php echo base_url()?>public/assets/vendor/datatables/css/normalize.css">
       
-      <script src="<?php echo base_url()?>public/assets/js/topbar.min.js"></script>
-
+      
       <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -55,4 +54,6 @@
       </head>
   <body>
     <div class="wrapper">
+    <script src="<?php echo base_url()?>public/assets/js/topbar.min.js"></script>
+
 
