665f65150f83f56933abd10a73555f0b5abeb255
Agung Kurniawan <cintarembo@gmail.com>
2017-12-27 12:33:29 +0700

    Merubah insert + edit sistem

--- application/modules/admin/controllers/Films.php
+++ application/modules/admin/controllers/Films.php
@@ -1,234 +1,267 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
 
 class Films extends MY_Controller
 {
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         if ($this->ion_auth->logged_in()) {
             if (!$this->ion_auth->is_admin()) {
                 redirect('pub/home');
             }
-        }else{
+        } else {
             redirect('pub/home');
         }
-        
+
         $this->load->model(array(
-            'films/Films_model'=>'film',
-            'films/Genre_model'=>'genre',
-            'films/Genrefilm_model' =>'gfm',
-            'films/Studio_model'=> 'studio'
+            'films/Films_model' => 'film',
+            'films/Genre_model' => 'genre',
+            'films/Genrefilm_model' => 'gfm',
+            'films/Studio_model' => 'studio',
+            'films/Jam_model' => 'jam',
+            'films/Film_studio_model' => 'fsm',
+            'films/jam_tayang_model' => 'jtm',
             ));
         $this->load->library(array(
             'form_validation' => 'validation',
-            'image_nation'    => 'image'
+            'image_nation' => 'image',
         ));
-        $this->load->helper('slug_helper','slug');
-        $this->validation->CI =& $this;
+        $this->load->helper('slug_helper', 'slug');
+        $this->validation->CI = &$this;
     }
 
     /**
-     * index
-     *
-     * @return void
+     * index.
      */
-    public function index(){
+    public function index()
+    {
         $this->data['film'] = $this->film->get_all();
         $this->render('films/films');
     }
 
     /**
-     * addFilms
-     *
-     * @return void
+     * addFilms.
      */
-    public function addFilms(){
-        if ($this->input->post()):
-            if ($this->validation->run('addFilms')==TRUE):
-                $judul     = $this->input->post('judul');
-                $tgl       = $this->input->post('tanggal-tayang');            
-                $jam       = $this->input->post('jam-tayang');
-                $durasi    = $this->input->post('durasi-film');
-                $harga     = $this->input->post('harga-tiket');
-                $sinopsis  = $this->input->post('sinopsis');
-                $genre     = $this->input->post('genrefilm');
-                $hall      = $this->input->post('studio');
-                $featured  = $this->input->post('featured');
-                $status    = $this->input->post('status');
-                
-                $config['upload_path']          = './public/uploads/original';
-                $config['allowed_types']        = 'gif|jpg|png';
-                $config['encrypt_name']         = TRUE;
-                $config['max_size']             = 10000;
-                $config['max_width']            = 4328;
-                $config['max_height']           = 4328;
+    public function addFilms()
+    {
+        if ($this->input->post()) {
+            if ($this->validation->run('addFilms') == true) {
+                $judul = $this->input->post('judul');
+                $sinopsis = $this->input->post('sinopsis');
+                $mulai_tayang = $this->input->post('mulai-tayang');
+                $selesai_tayang = $this->input->post('selesai-tayang');
+                $jam = $this->input->post('jam');
+                $durasi = $this->input->post('durasi-film');
+                $genre = $this->input->post('genrefilm');
+                $studio = $this->input->post('studio');
+                $featured = $this->input->post('featured');
+                $status = $this->input->post('status');
+
+                $config['upload_path'] = './public/uploads/original';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['encrypt_name'] = true;
+                $config['max_size'] = 10000;
+                $config['max_width'] = 4328;
+                $config['max_height'] = 4328;
                 $this->load->library('upload', $config);
                 $this->upload->do_upload('coverfilm');
-                
-                /** Resize images */
-                $this->image->source($this->upload->data('file_name'));
-                $this->image->process('255x220|380x600|424x424|526x773|400x420');
 
-                $data = array(
-                    'judul'          => $judul,
-                    'sinopsis'       => $sinopsis,
-                    'tanggal_tayang' => $tgl,
-                    'jam_tayang'     => $jam,
-                    'durasi'         => $this->number($durasi),
-                    'harga'          => $this->number($harga),
-                    'cover'          => $this->upload->data('file_name'),
-                    'id_hall'        => $hall,
-                    'featured'       => $feat = (!empty($featured)) ? '1' : '',
-                    'status'         => $sts  = (!empty($status)) ? '1' : '0',
-                    'slug'           => slug($judul)
-                    );
-                
+                /* Resize images */
+                if ($this->upload->do_upload('coverfilm')) {
+                    $this->image->source($this->upload->data('file_name'));
+                    $this->image->process('255x220|380x600|424x424|526x773|400x420');
+                }
+
+                $data = [
+                    'judul' => $judul,
+                    'sinopsis' => $sinopsis,
+                    'mulai_tayang' => $mulai_tayang,
+                    'selesai_tayang' => $selesai_tayang,
+                    'durasi' => $this->number($durasi),
+                    'cover' => $this->upload->data('file_name'),
+                    'featured' => $feat = (!empty($featured)) ? '1' : '',
+                    'status' => $sts = (!empty($status)) ? '1' : '0',
+                    'slug' => slug($judul),
+                ];
+
                 $this->film->insert($data);
-                $this->db->insert('tbl_genrefilm',array(
-                                    'id_film' => $this->db->insert_id(),
-                                    'id_genre'=> $genre));
-                
-                $status = array(
-                    'status'  => true,
-                    'message' => 'Berhasil menambahkan film baru'
-                );
+                $film = $this->film->get($this->db->insert_id());
+                $id = $film->id_film;
+                for ($i = 0; $i < count($genre); ++$i) {
+                    $this->db->insert('tbl_genrefilm', ['id_film' => $id, 'id_genre' => $genre[$i]]);
+                }
+                for ($i = 0; $i < count($studio); ++$i) {
+                    $this->db->insert('film_studio', ['id_film' => $id, 'id_studio' => $studio[$i]]);
+                }
+                for ($i = 0; $i < count($jam); ++$i) {
+                    $id_jam = $this->jam->where('jam', $jam[$i])->get()->id;
+                    $this->db->insert('jam_tayang', ['id_film' => $id, 'id_jam' => $id_jam]);
+                }
+                $status = [
+                    'status' => true,
+                    'msg' => 'Berhasil menambahkan film baru',
+                ];
                 echo json_encode($status);
-            else:
-                $err = $this->validation->error_array();
-                show_error($err);
-            endif; //.Form Validation*/
-        else:
+            } else {
+                $status = ['status' => false, 'msg' => 'Terjadi kesalahan, silahkan coba lagi'];
+                echo json_encode($status);
+            } //.Form Validation*/
+        } else {
             $this->data['genre'] = $this->genre->get_all();
-            $this->data['studio']= $this->studio->get_all();
+            $this->data['studio'] = $this->studio->get_all();
             $this->render('films/addFilms');
-        endif; //.this->input->post()
+        } //.this->input->post()
     }
 
     /**
      * editFilms
-     * Digunakan untuk edit/update pada film
+     * Digunakan untuk edit/update pada film.
+     *
      * @param mixed $id
-     * @return void
      */
-    public function editFilms($id = NULL){
-        if ($this->input->post()):
-            if ($this->validation->run('addFilms')==TRUE):
-                $judul  = $this->input->post('judul');
-                $tgl    = $this->input->post('tanggal-tayang');            
-                $jam    = $this->input->post('jam-tayang');
+    public function editFilms($id = null)
+    {
+        if ($this->input->post()) {
+            if ($this->validation->run('addFilms') == true) {
+                $id = $this->input->post('id');
+                $judul = $this->input->post('judul');
+                $sinopsis = $this->input->post('sinopsis');
+                $mulai_tayang = $this->input->post('mulai-tayang');
+                $selesai_tayang = $this->input->post('selesai-tayang');
+                $jam = $this->input->post('jam');
                 $durasi = $this->input->post('durasi-film');
-                $harga  = $this->input->post('harga-tiket');
-                $sinopsis  = $this->input->post('sinopsis');
-                $hall      = $this->input->post('studio');
-                $featured  = $this->input->post('featured');
-                $status    = $this->input->post('status');
+                $genre = $this->input->post('genrefilm');
+                $studio = $this->input->post('studio');
+                $featured = $this->input->post('featured');
+                $status = $this->input->post('status');
 
-                $config['upload_path']          = './public/uploads/original';
-                $config['allowed_types']        = 'gif|jpg|png';
-                $config['encrypt_name']         = TRUE;
-                $config['max_size']             = 100;
-                $config['max_width']            = 4328;
-                $config['max_height']           = 4328;
+                $config['upload_path'] = './public/uploads/original';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['encrypt_name'] = true;
+                $config['max_size'] = 10000;
+                $config['max_width'] = 4328;
+                $config['max_height'] = 4328;
                 $this->load->library('upload', $config);
-                $this->upload->do_upload('coverFilm');
+                $this->upload->do_upload('coverfilm');
 
-                /** Resize images */
-                $this->image->source($this->upload->data('file_name'));
-                $this->image->process('255x220|380x600|424x424|526x773|400x420');
+                /* Resize images */
+                if ($this->upload->do_upload('coverfilm')) {
+                    $this->image_nation->delete($id);
+                    $this->image->source($this->upload->data('file_name'));
+                    $this->image->process('255x220|380x600|424x424|526x773|400x420');
+                    $data = [
+                        'judul' => $judul,
+                        'sinopsis' => $sinopsis,
+                        'mulai_tayang' => $mulai_tayang,
+                        'selesai_tayang' => $selesai_tayang,
+                        'durasi' => $this->number($durasi),
+                        'cover' => $this->upload->data('file_name'),
+                        'featured' => $feat = (!empty($featured)) ? '1' : '',
+                        'status' => $sts = (!empty($status)) ? '1' : '0',
+                        'slug' => slug($judul),
+                    ];
+                } else {
+                    $data = [
+                        'judul' => $judul,
+                        'sinopsis' => $sinopsis,
+                        'mulai_tayang' => $mulai_tayang,
+                        'selesai_tayang' => $selesai_tayang,
+                        'durasi' => $this->number($durasi),
+                        'featured' => $feat = (!empty($featured)) ? '1' : '',
+                        'status' => $sts = (!empty($status)) ? '1' : '0',
+                        'slug' => slug($judul),
+                    ];
+                }
 
-                $data = array(
-                    'judul'          => $judul,
-                    'sinopsis'       => $sinopsis,
-                    'tanggal_tayang' => $tgl,
-                    'jam_tayang'     => $jam,
-                    'durasi'         => $this->number($durasi),
-                    'harga'          => $this->number($harga),
-                    'cover'          => $this->upload->data('file_name'),
-                    'id_hall'        => $hall,
-                    'featured'       => $feat = (!empty($featured)) ? '1' : '',
-                    'status'         => $sts  = (!empty($status)) ? '1' : '0',
-                    'slug'           => slug($judul)
-                    );
-                
-                $this->film->update($data,$id);
-                $status = array(
-                    'status'  => true,
-                    'message' => 'Berhasil menambahkan film baru'
-                );
+                $this->film->update($data, $id);
+                $this->film->update_genre($id, $genre);
+                $this->film->update_studio($id, $studio);
+                $this->film->update_jam($id, $jam);
+
+                $status = [
+                    'status' => true,
+                    'msg' => 'Berhasil melakukan pembaharuan film',
+                ];
                 echo json_encode($status);
-            else:
-                show_error(500);
-            endif; //.Form Validation
-        else:
-            $this->data['data'] = $this->gfm->with_film()->with_genre()->get($id);
+            } else {
+                echo json_encode($this->validation->error_array());
+            } //.Form Validation*/
+        } else {
+            $this->data['data'] = $this->gfm->with_film()->get($id);
             $this->data['genre'] = $this->genre->get_all();
-            $this->data['studio']= $this->studio->get_all();
+            $this->data['studio'] = $this->studio->get_all();
+            $this->data['jam'] = $this->jam->get_all();
+            $this->data['data_studio'] = $this->fsm->where('id_film', $id)->with_studio()->get_all();
+            $this->data['data_jam'] = $this->jtm->where('id_film', $id)->with_jam()->get_all();
             $this->render('films/editFilms');
-        endif; //.this->input->post()
+        } //.this->input->post()
     }
 
     /**
      * delFilms
-     * Delete film and images
-     * @return void
+     * Delete film and images.
      */
-    public function delFilms(){
+    public function delFilms()
+    {
         $id = $this->input->post('id');
         $data = $this->film->get($id);
-        if(file_exists($image = FCPATH.'public/uploads/original/'.$data->cover)):
+        if (file_exists($image = FCPATH.'public/uploads/original/'.$data->cover)) {
             $size = $this->config->item('default_sizes', 'image_nation');
-            $a = explode('|',$size);
+            $a = explode('|', $size);
             foreach ($a as $b) {
-                if(file_exists($img = FCPATH.'public/uploads/'.$b.'/'.$data->cover)){
+                if (file_exists($img = FCPATH.'public/uploads/'.$b.'/'.$data->cover)) {
                     unlink($img);
                 }
             }
             unlink($image);
-        endif;
+        }
         $this->film->delete($id);
     }
 
-    
     /**
-     * Genre
-     *
-     * @return void
+     * Genre.
      */
-    public function genre(){
+    public function genre()
+    {
         $this->data['genre'] = $this->genre->get_all();
         $this->render('films/genre');
     }
 
     /**
      * addgenre
-     * menambahkan genre baru
-     * @return void
+     * menambahkan genre baru.
      */
-    public function addgenre(){
-        if ($this->input->post()):
+    public function addgenre()
+    {
+        if ($this->input->post()) {
             $nama = $this->input->post('nama');
-            $this->genre->insert(array('genre'=>$nama));
-        else:
+            $this->genre->insert(array('genre' => $nama));
+        } else {
             show_404();
-        endif;
+        }
     }
 
     /**
      * editgenre
-     * digunakan untuk melakukan update/edit pada genre
-     * @return void
+     * digunakan untuk melakukan update/edit pada genre.
      */
-    public function editgenre(){
+    public function editgenre()
+    {
         $id = $this->input->post('id');
         $nama = $this->input->post('nama');
-        $this->genre->update(array('genre'=>$nama),$id);
+        $this->genre->update(array('genre' => $nama), $id);
     }
 
     /**
      * delgenre
-     * digunakan untuk menghapus genre
-     * @return void
+     * digunakan untuk menghapus genre.
      */
-    public function delgenre(){
+    public function delgenre()
+    {
         $id = $this->input->post('id');
         $this->genre->delete($id);
     }
