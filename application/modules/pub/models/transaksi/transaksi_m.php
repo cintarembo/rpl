b786e70d0534ecef8906c3dd85989e9bd6d0015c
Agung Kurniawan <cintarembo@gmail.com>
2018-01-24 13:39:28 +0700

    Menambahkan fungsi konfirmasi

--- application/modules/pub/models/transaksi/transaksi_m.php
+++ application/modules/pub/models/transaksi/transaksi_m.php
@@ -10,4 +10,35 @@ class Transaksi_m extends MY_Model
     {
         parent::__construct();
     }
+
+    public function konfirmasi()
+    {
+        $id = $this->input->post('id');
+        $nama = $this->input->post('nama');
+        $bank = $this->input->post('bank');
+
+        $config['upload_path'] = './public/uploads/original';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['encrypt_name'] = true;
+        $config['max_size'] = 10000;
+        $config['max_width'] = 4328;
+        $config['max_height'] = 4328;
+        $this->load->library('upload', $config);
+        $this->upload->do_upload('bukti');
+
+        /* Resize images */
+        $bukti = $this->upload->data('file_name');
+        if ($this->upload->do_upload('bukti')) {
+            $data = array(
+                'nama' =>$nama ,
+                'bank' =>$bank,
+                'bukti'=>$bukti,
+                'id_transaksi'=>$id
+            );
+            $this->db->insert('konfirmasi', $data);
+            $status = array('sts' => true, );
+        } else {
+            echo json_encode($this->upload->display_errors());
+        }
+    }
 }
